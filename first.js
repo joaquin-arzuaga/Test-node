0cec90727bd2f63c624c2ad768f39bc5580f542f
joaquin-arzuaga <joaquin.arzuaga@correo.ucu.edu.uy>
2025-03-17 23:19:52 -0300

    commit

--- first.js
+++ first.js
@@ -1,1 +1,14 @@
-console.log("hola mundo");
+console.log("Hello, World!");
+
+async function main() {
+
+    console.log("hello world from main");
+    const req = await fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings");
+    if (req.ok) {
+        const data = await req.json();
+        //console.log(data.docs[0].autor_name);
+        console.log(data);
+    }
+}
+
+main();
